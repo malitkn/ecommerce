5d109611d5c0ee55fde66824a973354802a9cc0c
malitkn <bsn.muhammetali@gmail.com>
2023-02-10 12:20:43 +0300

    Added Role Control

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -28,6 +28,9 @@ class AuthenticatedSessionController extends Controller
         $request->authenticate();
         $request->session()->regenerate();
 
+        if ($request->user()->role == "admin") {
+            return redirect()->intended(RouteServiceProvider::PANEL);
+        }
 
         return redirect()->intended(RouteServiceProvider::HOME);
     }

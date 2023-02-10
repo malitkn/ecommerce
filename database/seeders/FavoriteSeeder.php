da405caae8d227ff50f837bd30b73f0284f80d29
malitkn <bsn.muhammetali@gmail.com>
2023-02-10 23:09:21 +0300

    Favorites Added

---
+++ database/seeders/FavoriteSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Favorite;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class FavoriteSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Favorite::factory()->count(200)->create();
+    }
+}

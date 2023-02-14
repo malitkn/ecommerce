af5798d8032fce6d5d37aad45fe8ddab062bec9f
malitkn <bsn.muhammetali@gmail.com>
2023-02-14 22:33:34 +0300

    Revert "Smtp Table Removed"
    This reverts commit 75a29e9cc024c31ac3be6436eaa3a91c27b112c5.

---
+++ database/migrations/2023_02_07_182421_create_smtp_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('smtp', function (Blueprint $table) {
+            $table->id();
+            $table->string('server');
+            $table->string('email');
+            $table->string('username');
+            $table->string('password');
+            $table->integer('port');
+            $table->string('encryption');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('smtp');
+    }
+};
---
+++ resources/views/back/settings/smtp/index.blade.php
@@ -0,0 +1,10 @@
+@extends('back.layouts.master')
+@section('title','SMTP Ayarları')
+
+@section('content')
+
+@endsection
+
+@section('js')
+    <script src="{{ asset('back/assets/js/file-upload.js') }}"></script>
+@endsection
